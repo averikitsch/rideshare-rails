9b8fb96945181a257482bab7e287c4bb36140925
Tanja Stroble <plusdeltah@gmail.com>
2017-10-02 17:25:34 -0700

    added edit, show and destroy with views for passengers and drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,6 +5,32 @@ def index
   @drivers = Driver.all.order("created_at DESC")
 end
 
+def show
+
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+  if @driver.update(driver_params)
+    redirect_to drivers_path(@driver)
+  else
+    render "edit"
+  end
+end
+
+def destroy
+  @driver.destroy
+  redirect_to drivers_path
+end
+
 private
 
 def driver_params
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -6,6 +6,33 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all.order("created_at DESC")
   end
 
+  def show
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+  end
+
+  def edit
+  end
+
+  def update
+    if @passenger.update(passenger_params)
+      redirect_to passengers_path(@passenger)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @passenger.destroy
+    redirect_to passengers_path
+  end
+
   private
 
   def passenger_params
