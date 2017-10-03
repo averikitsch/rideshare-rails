abfffe8e16fca94d687ac10eba3e76b605762e4d
Tanja Stroble <plusdeltah@gmail.com>
2017-10-03 16:37:59 -0700

    cleaned up comments

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,11 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-# validates :driver_id, numericality: { greater_than: 0 }
-# validates :passenger_id,  numericality: { greater_than: 0 }
 validates :cost, presence: true, numericality: { greater_than: 100 }
 validates :rating, presence: true,numericality: { only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }
 
 end
-
-# does this need any defs?
