f8f8165ed04e1ac031bdd91593f2a8278520a559
Tanja Stroble <plusdeltah@gmail.com>
2017-10-03 16:05:53 -0700

    added avg rating, total trip cost and total earnings defs

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,4 +4,24 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence:true, uniqueness: true
 
+
+  def average_rating
+    total = 0.0
+    driver_trips = self.trips
+    trip_count = driver_trips.length
+    driver_trips.each do |trip|
+      total += trip.rating
+    end
+    return "#{(total / trip_count).round(2)}"
+  end
+
+  def total_earnings
+    total_earnings = 0
+    drivers_trips = self.trips
+    drivers_trips.each do |trip|
+      total_earnings += trip.cost
+    end
+    return "$ #{sprintf("%.2f",(total_earnings / 100.0))}"
+  end
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,8 +4,17 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_number, presence:true
 
+
+
+  def total_cost
+    total_cost = 0
+    passenger_trips = self.trips
+
+    passenger_trips.each do |trip|
+      total_cost += trip.cost
+    end
+
+    return "$ #{sprintf("%.2f",(total_cost / 100.0))}"
+  end
+
 end
-
-
-#needs trip cost
-#maybe something for rating?
